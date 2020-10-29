3be79cba16bcbba1e1e62e9d72ff71964a957000
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2020-10-30 09:50:52 +1100

    refactored and used different format.

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # scoreboard
 
-2 player scoreboard
-Limited to 50 points target.
-The winner will be hightlighted at the end of the game.
+1. 2 player scoreboard
+2. Limited to 50 points target.
+3. The winner will be hightlighted at the end of the game.
+
+4. Score can be entered manually or be selected from increment and decrement setting.
+5. All input field are validated. Enter field with information.
+   (EDITED VERSION )
+   The original version was created on a div alone.
+   Edited version was transformed into a table and form. This gives a better format for a responsive screen size and it reduces the code lines with less media querry.
+
+(Refratoring the code. )
+Refactored codes for multiple repeated format to one single objects.
+
+Applied ES6 arrow function
+
+Lean the main idea from Steel Colt simple scoreboard. Bootcamp
--- score.js
+++ score.js
@@ -1,124 +1,121 @@
-const play1Button = document.querySelector("#play1Button");
-const play2Button = document.getElementById("play2Button");
-const sco1 = document.querySelector("#sco1");
-const sco2 = document.getElementById("sco2");
-const inputTot = document.querySelector("#inputTot");
+// for refactoring create an object for both team/player
+//player name
+let name1 = "";
+let name2 = "";
+// creating objects for the 2 players
+const py1 = {
+  score: 0,
+  button: document.querySelector("#play1Button"),
+  display: document.querySelector("#sco1"),
+  nameInput: document.querySelector("#player1"),
+};
+
+// for refactoring create an object for both team/player 2
+const py2 = {
+  score: 0,
+  button: document.querySelector("#play2Button"),
+  display: document.querySelector("#sco2"),
+  nameInput: document.querySelector("#player2"),
+};
+
+const inputScore = document.getElementById("inputScore");
 const disWinScore = document.querySelector(".playFor");
+
+const submitBut = document.querySelector(".submitBut");
 const resetbut = document.getElementById("reset");
-const player1 = document.querySelector("#player1");
-const player2 = document.querySelector("#player2");
+
+// display winner message
 const winGame = document.querySelector(".winGame");
-const wordTo = document.querySelector("#wordTo");
-let p1score = 0;
-let p2score = 0;
+
+// counter to count the score
+
 let endGame = false;
-let chekIn = false;
+// let chekIn = false;
 let scoreWin = 0;
 
+// refactoring by creating  a generic function to support both players or teams information under one function
+const updatePlayerInfo = (player, opponent) => {
+  // function updatePlayerInfo(player, opponent) {
+  // check if game over
+  if (!endGame) {
+    player.score += 1;
+
+    if (player.score === scoreWin) {
+      endGame = true;
+      // winner score hightligted in green
+      // lost in red
+
+      player.display.classList.add("winner");
+      opponent.display.classList.add("lost");
+
+      winGame.classList.add("endMsg");
+    }
+  }
+  // sco1.textContent = p1score;
+  player.display.textContent = player.score;
+};
+
 // Button eventlistener for player 1
-if (!chekIn) {
-  play1Button.addEventListener("click", function () {
-    if (!endGame) {
-      p1score++;
-      // console.log(scoreWin, p1score, inputTot.value);
-      if (p1score === scoreWin) {
-        sco1.textContent = p1score;
-        sco1.classList.add("winner");
-        endGame = true; // game stop increment
-
-        // winner message
-        winGame.textContent = "WINNER !" + " " + player1.value;
-        winGame.classList.add("endMsg");
-
-        // win();
-      }
-      sco1.textContent = p1score;
-    }
-  });
-
-  // Button eventlistener for player 2
-
-  play2Button.addEventListener("click", function () {
-    if (!endGame) {
-      p2score++;
-      if (p2score === scoreWin) {
-        sco2.classList.add("winner");
-        endGame = true; // game stop increment
-        // winner message
-        winGame.textContent = "WINNER !" + " " + player2.value;
-        winGame.classList.add("endMsg");
-      }
-      sco2.textContent = p2score;
-    }
-  });
-}
-// score input field can be manualy change or using arrow
-
-inputTot.addEventListener("change", function () {
-  disWinScore.textContent = this.value;
-  scoreWin = +this.value; //string to number ES6
-  chekIn = true;
-
-  // disWinScore.textContent = inputTot.value;
-  // scoreWin = Number(inputTot.value);
-  resett();
+py1.button.addEventListener("click", function () {
+  updatePlayerInfo(py1, py2);
+  if (endGame) {
+    winGame.textContent = `WINNER ! ${name1}`;
+  }
+});
+// Button eventlistener for player 2
+py2.button.addEventListener("click", function () {
+  updatePlayerInfo(py2, py1);
+  if (endGame) {
+    winGame.textContent = `WINNER ! ${name2}`;
+  }
 });
 
-// button 1 names
-player1.addEventListener("change", function () {
-  play1Button.textContent = this.value;
+/*score input field can be manualy change or using arrow
+  Enter player details
+ button 1 names */
+py1.nameInput.addEventListener("change", function () {
+  py1.button.textContent = this.value;
+  name1 = this.value;
 });
 
 // button 2 names
-player2.addEventListener("change", function () {
-  play2Button.textContent = this.value;
+py2.nameInput.addEventListener("change", function () {
+  py2.button.textContent = this.value;
+  name2 = this.value;
+});
+// Selection Target score
+
+inputScore.addEventListener("change", function () {
+  // display target score
+  disWinScore.textContent = this.value;
+  scoreWin = +this.value; //string to number ES6
+});
+// RESET BUTTON SELECTED
+
+resetbut.addEventListener("click", function () {
+  resett();
 });
 
 // reset all input and out fields
-resetbut.addEventListener("click", function () {
-  inputTot.value = 0;
-  disWinScore.textContent = 0;
-  chekIn = false;
-  resett();
-  // console.log("reset click");
-});
-
-// action for function reset...initialize fields.
 function resett() {
-  p1score = 0;
-  p2score = 0;
-  sco1.textContent = 0;
-  sco2.textContent = 0;
-  sco1.classList.remove("winner");
-  sco2.classList.remove("winner");
-  winGame.classList.remove("endMsg");
   endGame = false;
-  play2Button.textContent = "Player 2";
-  play1Button.textContent = "Player 1";
-  player2.value = "";
-  player1.value = "";
+
+  py1.score = 0;
+  py2.score = 0;
+  py1.score.textContent = 0;
+  py2.score.textContent = 0;
+
+  py1.score.classList.remove("winner", "lost");
+  py2.score.classList.remove("winner", "lost");
+
+  winGame.classList.remove("endMsg");
+
+  py1.button.textContent = "Player 2";
+  py2.button.textContent = "Player 1";
+  inputScore.value = 0;
+
+  name1 = "";
+  name2 = "";
+
   winGame.textContent = "";
 }
-
-// function myFunction(butt) {
-//   if (butt.matches) {
-//     // If media query matches
-
-//     document.body.style.backgroundColor = "yellow";
-//   } else {
-//     document.body.style.backgroundColor = "pink";
-//   }
-// }
-
-// const butt = window.matchMedia("(max-width: 740px)");
-// myFunction(butt); // Call listener function at run time
-// butt.addListener(myFunction); // Attach listener function on state changes
-
-// function win() {
-//   winGame.textContent = "WINNER !";
-// }
-// console.log(resetbut);
-//  SEE THE VALUE IN CONSOLE
-// let input = document.querySelector("input");
-// input.value
-// " 2"
